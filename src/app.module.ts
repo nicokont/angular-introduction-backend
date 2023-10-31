90edd20b647c85211248dfdda8bc38f9e1666af9
nicokont <nicokont@gmail.com>
2023-10-31 19:49:15 +0200

    Atlas connection and population

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb+srv://cfUser:hZRvIf9Ggipcwlvf@cluster0.rvfsxyg.mongodb.net/?retryWrites=true&w=majority'), UserModule],
+  imports: [ConfigModule.forRoot(), MongooseModule.forRoot(process.env.MONGODB_URI), UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
