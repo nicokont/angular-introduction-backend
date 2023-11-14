91a439c9148acdaa271299d167efbebb8b380e4e
nicokont <nicokont@gmail.com>
2023-11-14 18:00:18 +0200

    jwt authentication

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,13 @@
+import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
+import { AuthDto } from './auth.dto';
+import { AuthService } from './auth.service';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('login')
+  async login(@Body(new ValidationPipe()) credentials: AuthDto): Promise<{access_token: string}> {
+    return this.authService.login(credentials)
+  }
+}
